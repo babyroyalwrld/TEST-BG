9a67f9cddf2f998574276ecb8759d50881bfa94a
babyroyalwrld <168953694+babyroyalwrld@users.noreply.github.com>
2025-01-21 04:58:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["", "Watching A Movie"];
+const statusMessages = ["Watching A Movie", "Playing Fortnite"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
